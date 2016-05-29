ae2e24bee35ab5a67e641b45414644c62f7ce52b
Francesco Tonini <francescoantoniotonini@gmail.com>
2016-05-29 21:51:41 +0200

    New create_db script

--- create_db.sql
+++ create_db.sql
@@ -27,43 +27,44 @@ CREATE TABLE `agency` (
 DROP TABLE IF EXISTS calendar;
 
 CREATE TABLE `calendar` (
-    service_id VARCHAR(25),
-	monday TINYINT(1),
-	tuesday TINYINT(1),
-	wednesday TINYINT(1),
-	thursday TINYINT(1),
-	friday TINYINT(1),
-	saturday TINYINT(1),
-	sunday TINYINT(1),
-	start_date VARCHAR(8),	
-	end_date VARCHAR(8),
+    service_id VARCHAR(25) PRIMARY KEY NOT NULL,
+	monday TINYINT(1) NOT NULL,
+	tuesday TINYINT(1) NOT NULL,
+	wednesday TINYINT(1) NOT NULL,
+	thursday TINYINT(1) NOT NULL,
+	friday TINYINT(1) NOT NULL,
+	saturday TINYINT(1) NOT NULL,
+	sunday TINYINT(1) NOT NULL,
+	start_date VARCHAR(8) NOT NULL,	
+	end_date VARCHAR(8) NOT NULL,
 	KEY `service_id` (service_id)
 );
 
 DROP TABLE IF EXISTS calendar_dates;
 
 CREATE TABLE `calendar_dates` (
-    service_id VARCHAR(25),
-    `date` VARCHAR(8),
-    date_timestamp INT(11),
-    exception_type INT(2),
+    service_id VARCHAR(25) NOT NULL,
+    `date` VARCHAR(8) NOT NULL,
+    exception_type INT(2) NOT NULL,
     KEY `service_id` (service_id),
-    KEY `date_timestamp` (date_timestamp),
+    KEY `date` (`date`),
     KEY `exception_type` (exception_type)    
 );
 
 DROP TABLE IF EXISTS routes;
 
 CREATE TABLE `routes` (
-    route_id INT(11) PRIMARY KEY,
+    route_id INT(11) PRIMARY KEY NOT NULL,
 	agency_id VARCHAR(11),
-	route_short_name VARCHAR(50),
-	route_long_name VARCHAR(255),
-	route_type INT(2),
+	route_short_name VARCHAR(255) NOT NULL,
+	route_long_name VARCHAR(255) NOT NULL,
+	route_type INT(2) NOT NULL,
 	route_text_color VARCHAR(255),
 	route_color VARCHAR(255),
 	route_url VARCHAR(255),
 	route_desc VARCHAR(255),
+	route_sequence TINYINT(1) NOT NULL,
+	city VARCHAR(2) NOT NULL,
 	KEY `agency_id` (agency_id),
 	KEY `route_type` (route_type)
 );
@@ -71,11 +72,11 @@ CREATE TABLE `routes` (
 DROP TABLE IF EXISTS stop_times;
 
 CREATE TABLE `stop_times` (
-    trip_id VARCHAR(25),
-	arrival_time VARCHAR(8),
-	departure_time VARCHAR(8),
-	stop_id INT(11),
-	stop_sequence INT(11),
+    trip_id VARCHAR(25) NOT NULL,
+	arrival_time VARCHAR(8) NOT NULL,
+	departure_time VARCHAR(8) NOT NULL,
+	stop_id INT(11) PRIMARY KEY NOT NULL,
+	stop_sequence INT(11) NOT NULL,
 	stop_headsign VARCHAR(50),
 	pickup_type INT(2),
 	drop_off_type INT(2),
@@ -90,12 +91,12 @@ CREATE TABLE `stop_times` (
 DROP TABLE IF EXISTS stops;
 
 CREATE TABLE `stops` (
-    stop_id INT(11) PRIMARY KEY,
+    stop_id INT(11) PRIMARY KEY NOT NULL,
     stop_code VARCHAR(50),
-	stop_name VARCHAR(255),
+	stop_name VARCHAR(255) NOT NULL,
 	stop_desc VARCHAR(255),
-	stop_lat DECIMAL(8,6),
-	stop_lon DECIMAL(8,6),
+	stop_lat DECIMAL(8,6) NOT NULL,
+	stop_lon DECIMAL(8,6) NOT NULL,
 	zone_id INT(11),
 	stop_url VARCHAR(255),
 	location_type INT(2),
@@ -110,6 +111,11 @@ CREATE TABLE `stops` (
 DROP TABLE IF EXISTS trips;
 
 CREATE TABLE `trips` (
+	route_id INT(11) NOT NULL,
+	service_id VARCHAR(25) NOT NULL,
+	trip_id VARCHAR(25) PRIMARY KEY NOT NULL,
+	trip_headsign VARCHAR(255),
+	trip_short_name VARCHAR(255),
 	direction_id TINYINT(1),
 	block_id INT(11),
 	shape_id INT(11),
@@ -117,12 +123,7 @@ CREATE TABLE `trips` (
 	bikes_allowed TINYINT(1),
 	KEY `route_id` (route_id),
 	KEY `service_id` (service_id),
-    route_id INT(11),
 	KEY `direction_id` (direction_id),
-	service_id VARCHAR(25),
-	trip_id VARCHAR(25) PRIMARY KEY,
-	trip_headsign VARCHAR(255),
-	trip_short_name VARCHAR(255),
 	KEY `block_id` (block_id),
 	KEY `shape_id` (shape_id)
 );
@@ -130,8 +131,8 @@ CREATE TABLE `trips` (
 DROP TABLE IF EXISTS transfers;
 
 CREATE TABLE `transfers` (
-	from_stop_id INT(11),
-	to_stop_id INT(11),
-	transfer_type TINYINT(1),
+	from_stop_id INT(11) NOT NULL,
+	to_stop_id INT(11) NOT NULL,
+	transfer_type TINYINT(1) NOT NULL,
 	min_transfer_time INT(11)
 );
